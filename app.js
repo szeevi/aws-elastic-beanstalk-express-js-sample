67294b67813bfc0a4eacf3514cfbc5bfbbdc015b
szeevi <66994877+szeevi@users.noreply.github.com>
2021-01-01 10:42:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World from Shlomi! Hellow from AWS'));
+app.get('/', (req, res) => res.send('2020.11.298840.0'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
